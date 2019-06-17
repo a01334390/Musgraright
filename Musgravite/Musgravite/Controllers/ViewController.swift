f23d7a03e401e615483e9b85faa19e97c6fc9ff9
Fernando Martin Garcia Del Angel <a01334390@itesm.mx>
2019-06-17 17:40:35 -0500

    Added Authentication using FirebaseUI

--- Musgravite/Musgravite/Controllers/ViewController.swift
+++ Musgravite/Musgravite/Controllers/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func loginTapped(_ sender: Any) {
+        // Get the default auth UI Object
+        let authUI = FUIAuth.defaultAuthUI()
+        guard authUI != nil else {
+            return
+        }
+        // Set ourselves as the delegate
+        authUI?.delegate = self
+        authUI?.providers = [FUIEmailAuth()]
+        // Get a reference to the auth UI view controller
+        let authViewController = authUI!.authViewController()
+        
+        // Show it
+        present(authViewController, animated: true, completion: nil)
+    }
+    
+}
 
+extension ViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        // Check if there was an error
+        guard error == nil else {
+            // Log the error
+            print(error ?? "An error has occured in Musgravite")
+            return
+        }
+        
+        //authDataResult?.user.uid
+        
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
 }
 
---
+++ Musgravite/Musgravite/Extensions/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  Musgravite
+//
+//  Created by Fernando Martin Garcia Del Angel on 6/17/19.
+//  Copyright © 2019 Aabo Technologies. All rights reserved.
+//
+
+import UIKit
