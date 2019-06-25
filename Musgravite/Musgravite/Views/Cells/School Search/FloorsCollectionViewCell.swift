f9e6062a5fad74f2ab07fb74175aacda5ffe9318
Fernando Martin Garcia Del Angel <a01334390@itesm.mx>
2019-06-24 21:46:25 -0500

    Restored original search

---
+++ Musgravite/Musgravite/Views/Cells/School Search/FloorsCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FloorsCollectionViewCell.swift
+//  Musgravite
+//
+//  Created by Fernando Martin Garcia Del Angel on 6/24/19.
+//  Copyright © 2019 Aabo Technologies. All rights reserved.
+//
+
+import UIKit
+
+class FloorsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var bigImage: UIImageView!
+    @IBOutlet weak var bigTitle: UILabel!
+    
+}
---
+++ Musgravite/Musgravite/Views/Cells/School Search/RoomsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RoomsTableViewCell.swift
+//  Musgravite
+//
+//  Created by Fernando Martin Garcia Del Angel on 6/24/19.
+//  Copyright © 2019 Aabo Technologies. All rights reserved.
+//
+
+import UIKit
+
+class RoomsTableViewCell: UITableViewCell {
+    @IBOutlet weak var labname: UILabel!
+    @IBOutlet weak var buildingname: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
